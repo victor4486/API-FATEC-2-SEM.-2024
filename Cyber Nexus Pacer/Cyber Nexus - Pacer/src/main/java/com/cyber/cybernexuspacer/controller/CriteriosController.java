177786a433b4babbbd50caece64d8edf63f84130
Tiago Santos <tiagoalbertoo17@gmail.com>
2024-10-27 13:35:33 -0300

    Apresentando os critérios salvos na inicialização do programa

--- Cyber Nexus Pacer/Cyber Nexus - Pacer/src/main/java/com/cyber/cybernexuspacer/controller/CriteriosController.java
+++ Cyber Nexus Pacer/Cyber Nexus - Pacer/src/main/java/com/cyber/cybernexuspacer/controller/CriteriosController.java
@@ -1,6 +1,7 @@
 package com.cyber.cybernexuspacer.controller;
 
 import com.cyber.cybernexuspacer.dao.CriterioDao;
+import com.cyber.cybernexuspacer.entity.Criterio;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,6 +13,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class CriteriosController {
 
@@ -48,8 +51,6 @@ public class CriteriosController {
     @FXML
     private AnchorPane campo_criterios;// ScrollPane para exibir os critérios
 
-
-
     private int sprintCount = 0; // Contador para o número de sprints
 
     @FXML
@@ -58,15 +59,15 @@ public class CriteriosController {
     }
 
     @FXML
-    private TextField tituloCritério; // TextField para o título do critério
+    private TextField tituloCriterio; // TextField para o título do critério
 
     @FXML
-    private TextField descricaoCritério; // TextField para a descrição do critério
+    private TextField descricaoCriterio; // TextField para a descrição do critério
 
     @FXML
     private void handleAdicionarCriterio() {
-        String titulo = tituloCritério.getText();
-        String descricao = descricaoCritério.getText();
+        String titulo = tituloCriterio.getText();
+        String descricao = descricaoCriterio.getText();
         //caso campo vazio
         if (titulo.isEmpty() || descricao.isEmpty()) {
             System.out.println("Os campos de título e descrição não podem estar vazios.");
@@ -78,12 +79,15 @@ public class CriteriosController {
         CriterioDao criterioDao = new CriterioDao();
         criterioDao.inserirCriterio(titulo, descricao);
 
-        //criação do campo de exibição do critério
+    }
+
+    private void exibirCriterio(String titulo, String descricao) {
+        // Criação do campo de exibição do critério
         Pane novoCriterio = new Pane();
-        novoCriterio.setPrefSize(445, 50);
-        novoCriterio.setStyle("-fx-background-color: #86B6DD; -fx-background-radius: 4;");
+        novoCriterio.setPrefSize(445, 70);
         novoCriterio.setLayoutX(4);
-        novoCriterio.setLayoutY(4);
+        novoCriterio.setLayoutY(10);
+        novoCriterio.setStyle("-fx-background-color: #86B6DD; -fx-background-radius: 4;");
 
         // Adicionando o título e a descrição no critério
         Label tituloLabel = new Label("Título:   " + titulo);
@@ -94,6 +98,8 @@ public class CriteriosController {
         Label descricaoLabel = new Label("Descrição: " + descricao);
         descricaoLabel.setLayoutX(10);
         descricaoLabel.setLayoutY(30);
+        descricaoLabel.setWrapText(true);  // Permite quebra de linha
+        descricaoLabel.setPrefWidth(400);  // Defina uma largura preferencial
         descricaoLabel.setStyle("-fx-text-fill: white; -fx-font-size: 12px;");
 
         novoCriterio.getChildren().addAll(tituloLabel, descricaoLabel);
@@ -102,18 +108,31 @@ public class CriteriosController {
         int numeroDeCriterios = campo_criterios.getChildren().size();
 
         // Calcula a posição Y para o novo critério
-        double novaPosicaoY = numeroDeCriterios == 0 ? 0 : numeroDeCriterios * 58;
+        double novaPosicaoY = numeroDeCriterios == 0 ? 0 : numeroDeCriterios * 78;
 
         // Define a posição do novo critério
         novoCriterio.setLayoutY(novaPosicaoY);
 
         // Adiciona o novo critério ao AnchorPane (campo_criterios)
         campo_criterios.getChildren().add(novoCriterio);
-
-        // Limpa os campos de texto para adicionar novos critérios
-        tituloCritério.clear();
-        descricaoCritério.clear();
     }
+
+    private void carregarCriterios() throws SQLException {
+        CriterioDao criterioDao = new CriterioDao();
+        List<Criterio> criterios = criterioDao.listarCriterios();
+
+        for (Criterio criterio : criterios) {
+            exibirCriterio(criterio.geetTitulo(), criterio.getDescricao());
+        }
+    }
+
+    @FXML
+    public void initialize() throws SQLException {
+
+        carregarCriterios();
+    }
+
+
     @FXML
     public void handleSair(ActionEvent actionEvent) throws IOException {
         Main.setRoot("TelaMenu-view");
