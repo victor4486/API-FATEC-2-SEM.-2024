a31a495bb263ee39df72387d3c821a55b1ecd6e6
TiagoAugustoSeixas <tiago.assilva@hotmail.com>
2024-11-01 20:32:59 -0300

    Resolvendo conflitos

--- Cyber Nexus Pacer/Cyber Nexus - Pacer/src/main/java/com/cyber/cybernexuspacer/dao/ConexaoDao.java
+++ Cyber Nexus Pacer/Cyber Nexus - Pacer/src/main/java/com/cyber/cybernexuspacer/dao/ConexaoDao.java
@@ -5,32 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoDao {
-    //private static final String url = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:6543/postgres"; // Substitua pelos seus dados do Supabase
-    //private static final String user = "postgres.avytuuhwmswsbshvwipz"; // Substitua pelo usuário do Supabase
-    //private static final String password = "CyberNexusFatec"; // Substitua pela senha do Supabase
-
-
-
-    private static final String url = "jdbc:mysql://localhost:3306/pacerapi?useTimezone=true&serverTimezone=UTC"; // Substitua pelos seus dados do Supabase
-    private static final String user = "root"; // Substitua pelo usuário do Supabase
-    private static final String password = "*******"; // Substitua pela senha do Supabase
+    private static final String url = "jdbc:mysql://localhost:3306/pacerapi?useTimezone=true&serverTimezone=UTC";
+    private static final String user = "root";
+    private static final String password = "your_password_here";
 
     private static Connection conn;
 
     public static Connection getConnection() {
         try {
-            if (conn == null) {
-                Class.forName("org.postgresql.Driver"); // Atualizado para o driver PostgreSQL
+            if (conn == null || conn.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver"); // Atualizado para o driver MySQL
                 conn = DriverManager.getConnection(url, user, password);
-                return conn;
-            } else {
-                return conn;
+                System.out.println("Conexão estabelecida com sucesso!");
             }
+            return conn;
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.err.println("Falha ao conectar ao banco de dados: " + e.getMessage());
             return null;
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            System.err.println("Driver JDBC não encontrado: " + e.getMessage());
+            return null;
         }
     }
 
